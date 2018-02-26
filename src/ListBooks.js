56fceb2cdb80f7b14cee36f305e5b5467b5d789a
Diego R. Palacios Lepore <diegopalacios3@gmail.comgit>
2018-02-26 13:59:41 -0400

    Main Page meets the requirements and also the routing based on the project rubric.

---
+++ src/ListBooks.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+
+class ListBooks extends Component {
+
+    state = {
+        value: this.props.shelfStatus
+    }
+
+    render() {
+        return(
+            <ol className="books-grid">
+            {this.props.books.map((book) => (
+                this.props.shelfStatus === book.shelf &&(
+                <li key={book.id}>
+                    <div className="book">
+                        <div className="book-top">
+                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                        <div className="book-shelf-changer">
+                            <select value={this.state.value} onChange={(event)=> this.props.onChangeToShelf(book, event.target.value)}>  
+                                <option value="none" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                            </select>
+                        </div>
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{book.authors.map((author)=>( author ))}</div>
+                    </div>
+                </li>
+                )
+            ))}</ol>
+        )
+    }
+}
+
+export default ListBooks
